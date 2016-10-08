b691ae574062b07bb3e3c6130524c07d361d9bd0
darknawg63@gmail.com <darknawg63@gmail.com>
2016-10-08 21:06:15 +0200

    pset7: portfolio 1

--- public/index.php
+++ public/index.php
@@ -1,9 +1,21 @@
 <?php
 
     // configuration
-    require("../includes/config.php"); 
+    require("../includes/config.php");
+
+    // retrieve user's balance
+    $cash = CS50::query("SELECT cash FROM users WHERE id=?", $_SESSION["id"]);
+
+    if ($cash == 0)
+    {
+        $cash[0]["cash"] = 0;
+    }
+
+    // We would like to format in US Dollars with a Dollar sign
+    setlocale(LC_MONETARY, 'en_US.UTF-8');
 
     // render portfolio
-    render("portfolio.php", ["title" => "Portfolio"]);
+    render("portfolio.php", ["title" => "Portfolio", "id" => $_SESSION["id"],
+             "cash" => money_format('%.2n',$cash[0]["cash"])]);
 
 ?>
--- views/portfolio.php
+++ views/portfolio.php
@@ -1,3 +1,25 @@
-<div>
-    <iframe allowfullscreen frameborder="0" height="315" src="https://www.youtube.com/embed/oHg5SJYRHA0?autoplay=1&iv_load_policy=3&rel=0" width="420"></iframe>
+<div id="middle">
+  <table class="table table-striped">
+
+    <thead>
+      <tr>
+        <th>Symbol</th>
+        <th>Name</th>
+        <th>Shares</th>
+        <th>Price</th>
+        <th>TOTAL</th>
+      </tr>
+    </thead>
+
+    <tbody>
+
+      <tr><td>MSFT</td><td>Microsoft Corporation</td><td>50</td><td>$57.80</td><td>$2,890.00</td></tr>
+      <tr>
+        <td colspan="4">CASH</td>
+        <td><?= $cash ?></td>
+      </tr>
+
+    </tbody>
+
+  </table>
 </div>
