f9b35b3d92211405a911a49e9935996dddcbf078
darknawg63@gmail.com <darknawg63@gmail.com>
2016-10-09 11:09:18 +0200

    pset7: portfolio 4

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
     // configuration
     require("../includes/config.php");
 
-    // retrieve user's balance
+    // retrieve current user's balance
     $cash = CS50::query("SELECT cash FROM users WHERE id=?", $_SESSION["id"]);
 
     if ($cash == 0)
@@ -11,11 +11,29 @@
         $cash[0]["cash"] = 0;
     }
 
+    // retrieve current user's positions (stocks)
+    $rows = CS50::query("SELECT id, user_id, symbol, shares FROM portfolios WHERE user_id = ?", $_SESSION["id"]);
+
+    $positions = [];
+
+    foreach ($rows as $row)
+    {
+        $stock = lookup($row["symbol"]);
+        if ($stock !== false)
+        {
+            $positions[] = [
+                "name" => $stock["name"],
+                "price" => $stock["price"],
+                "shares" => $row["shares"],
+                "symbol" => $row["symbol"]
+            ];
+        }
+    }
     // We would like to format in US Dollars with a Dollar sign
     setlocale(LC_MONETARY, 'en_US.UTF-8');
 
     // render portfolio
-    render("portfolio.php", ["title" => "Portfolio", "id" => $_SESSION["id"],
-             "cash" => money_format('%.2n',$cash[0]["cash"])]);
+    render("portfolio.php", ["title" => "Portfolio", "id" => $_SESSION["id"], 
+        "cash" => money_format('%.2n',$cash[0]["cash"])]);
 
 ?>
