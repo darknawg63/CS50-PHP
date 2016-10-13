703b41f98869964a133b7bb4c35286e03de40161
darknawg63@gmail.com <darknawg63@gmail.com>
2016-10-13 17:53:39 +0200

    pset7: sell 2

--- public/sell.php
+++ public/sell.php
@@ -8,24 +8,31 @@
     // if user reached page via GET (as by clicking a link or via redirect)
     if ($_SERVER["REQUEST_METHOD"] == "GET")
     {
-        // else render form
-        render("sell_form.php", ["title" => "Sell Stuff"]);
-    }
-
-    // else if user reached page via POST (as by submitting a form via POST)
-    else if ($_SERVER["REQUEST_METHOD"] == "POST")
-    {
         // Round up all of the clients assets
         $assets = CS50::query("SELECT symbol, shares FROM portfolios WHERE user_id = ?", $_SESSION["id"]);
         if (empty($assets))
             apologize("No assets found.");
         else
-        if (!empty($assets))
+            render("sell_form.php", ["title" => "Sell Stuff", "assets" => $assets]);
+    }
+
+    // We would like to format in US Dollars with a Dollar sign
+    setlocale(LC_MONETARY, 'en_US.UTF-8');
+
+    // if user reached page via POST (as by submitting a form via POST)
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        if (isset($_POST["symbol"]))
         {
-            render("sell_form.php", ["title" => "Sell stuff", "assets" => $assets]);
+            $symbol = $_POST["symbol"];
+            $stock = lookup($symbol);
+
+            // Get the client's number of shares of the stock
+            $shares = CS50::query("SELECT shares FROM portfolios WHERE user_id = ?", $_SESSION["id"]);
+
+            // Update client's portfolio.
+            $shrs = CS50::query("UPDATE portfolios SET shares=NULL WHERE user_id = ?", $_SESSION["id"]);
+            $csh = CS50::query("UPDATE users SET cash= ? WHERE user_id = ?", (money_format('%.2n', $stock["price"]) * $shares), $_SESSION["id"]);
         }
     }
-    // Notice: Undefined variable: assets in /home/darknawg63/Code/PHP/cs50/pset7/views/sell_form.php
-    // After continuously getting the above error, it occurred to me that the query was only executed
-    // during a post
 ?>
